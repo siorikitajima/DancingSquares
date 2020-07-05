76a62742add3e2662d4a1b0fea322221ce9be961
siorikitajima <siorikitajima@sioris-imac.lan>
2020-07-05 06:11:09 -0700

    Added Left wallker and four colors

--- crowdWalking/animation.js
+++ crowdWalking/animation.js
@@ -1,3 +1,5 @@
+$(document).ready( function(){
+
 var width = $(window).width();
 var randomDelay = [
   Math.random() * 10000,
@@ -12,92 +14,168 @@ var randomDelay = [
   Math.random() * 10000
 ];
 
-anime({
+// Timeline for Top Row from Right
+var walkerlistR = anime.timeline({
+  easing: 'linear',
+  loop: true,
+});
+
+walkerlistR
+.add({
   targets: '.girl1R',
   translateX: -(width*1.2),
-  easing: 'linear',
-  duration: width*1.2*5,
-  loop: true,
-  delay: randomDelay[0]
-});
-
-anime({
-  targets: '.girl1L',
-  translateX: width*1.2,
-  easing: 'linear',
-  duration: width*1.2*5,
-  loop: true,
-  delay: randomDelay[1]
-});
-
-anime({
+  duration: width*1.2*5
+})
+.add({
   targets: '.girl2R',
   translateX: -(width*1.2),
-  easing: 'linear',
-  duration: width*1.2*4,
-  loop: true,
-  delay: randomDelay[2]
-});
-
-anime({
-  targets: '.girl2L',
-  translateX: width*1.2,
-  easing: 'linear',
-  duration: width*1.2*4,
-  loop: true,
-  delay: randomDelay[3]
-});
-
-anime({
+  duration: width*1.2*4.6,
+  //offset: randomDelay[1],
+},'-=3600')
+.add({
   targets: '.girl3R',
   translateX: -(width*1.2),
-  easing: 'linear',
-  duration: width*1.2*3,
-  loop: true,
-  delay: randomDelay[4]
-});
-
-anime({
-  targets: '.girl3L',
-  translateX: width*1.2,
-  easing: 'linear',
-  duration: width*1.2*3,
-  loop: true,
-  delay: randomDelay[5]
-});
-
-anime({
+  duration: width*1.2*4.8,
+  //offset: randomDelay[2],
+}, '-=2500')
+.add({
   targets: '.girl4R',
   translateX: -(width*1.2),
-  easing: 'linear',
-  duration: width*1.2*2.5,
-  loop: true,
-  delay: randomDelay[6]
-});
-
-anime({
-  targets: '.girl4L',
-  translateX: width*1.2,
-  easing: 'linear',
-  duration: width*1.2*2.5,
-  loop: true,
-  delay: randomDelay[7]
-});
-
-anime({
+  duration: width*1.2*5.2,
+  //offset: randomDelay[3],
+}, '-=4500')
+.add({
   targets: '.girl5R',
   translateX: -(width*1.2),
+  duration: width*1.2*5.4,
+  //offset: randomDelay[4],
+}, '-=2000');
+
+// Timeline for Top Row from Left
+var walkerlistL = anime.timeline({
   easing: 'linear',
-  duration: width*1.2*2,
   loop: true,
-  delay: randomDelay[8]
 });
 
-anime({
+walkerlistL
+.add({
+  targets: '.girl1L',
+  translateX: width*1.2,
+  duration: width*1.2*5
+})
+.add({
+  targets: '.girl2L',
+  translateX: width*1.2,
+  duration: width*1.2*4.6,
+  //offset: randomDelay[1],
+},'-=4600')
+.add({
+  targets: '.girl3L',
+  translateX: width*1.2,
+  duration: width*1.2*4.8,
+  //offset: randomDelay[2],
+}, '-=2400')
+.add({
+  targets: '.girl4L',
+  translateX: width*1.2,
+  duration: width*1.2*5.2,
+  //offset: randomDelay[3],
+}, '-=3900')
+.add({
   targets: '.girl5L',
   translateX: width*1.2,
-  easing: 'linear',
-  duration: width*1.2*2,
-  loop: true,
-  delay: randomDelay[9]
+  duration: width*1.2*5.4,
+  //offset: randomDelay[4],
+}, '-=2200');
+
 });
+
+// anime({
+//   targets: '.girl1R',
+//   translateX: -(width*1.2),
+//   easing: 'linear',
+//   duration: width*1.2*5,
+//   loop: true,
+//   delay: randomDelay[0]
+// });
+
+// anime({
+//   targets: '.girl1L',
+//   translateX: width*1.2,
+//   easing: 'linear',
+//   duration: width*1.2*5,
+//   loop: true,
+//   delay: randomDelay[1]
+// });
+
+// anime({
+//   targets: '.girl2R',
+//   translateX: -(width*1.2),
+//   easing: 'linear',
+//   duration: width*1.2*4,
+//   loop: true,
+//   delay: randomDelay[2]
+// });
+
+// anime({
+//   targets: '.girl2L',
+//   translateX: width*1.2,
+//   easing: 'linear',
+//   duration: width*1.2*4,
+//   loop: true,
+//   delay: randomDelay[3]
+// });
+
+// anime({
+//   targets: '.girl3R',
+//   translateX: -(width*1.2),
+//   easing: 'linear',
+//   duration: width*1.2*3,
+//   loop: true,
+//   delay: randomDelay[4]
+// });
+
+// anime({
+//   targets: '.girl3L',
+//   translateX: width*1.2,
+//   easing: 'linear',
+//   duration: width*1.2*3,
+//   loop: true,
+//   delay: randomDelay[5]
+// });
+
+// anime({
+//   targets: '.girl4R',
+//   translateX: -(width*1.2),
+//   easing: 'linear',
+//   duration: width*1.2*2.5,
+//   loop: true,
+//   delay: randomDelay[6]
+// });
+
+// anime({
+//   targets: '.girl4L',
+//   translateX: width*1.2,
+//   easing: 'linear',
+//   duration: width*1.2*2.5,
+//   loop: true,
+//   delay: randomDelay[7]
+// });
+
+// anime({
+//   targets: '.girl5R',
+//   translateX: -(width*1.2),
+//   easing: 'linear',
+//   duration: width*1.2*2,
+//   loop: true,
+//   delay: randomDelay[8]
+// });
+
+// anime({
+//   targets: '.girl5L',
+//   translateX: width*1.2,
+//   easing: 'linear',
+//   duration: width*1.2*2,
+//   loop: true,
+//   delay: randomDelay[9]
+// });
--- crowdWalking/walkers.js
+++ crowdWalking/walkers.js
@@ -1,79 +1,119 @@
-var girl1 = bodymovin.loadAnimation({
-    container: document.getElementById('girl1'),
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
-    path: 'girl6.json'
-  })
+$(document).ready( function(){
   
-var girl2 = bodymovin.loadAnimation({
-    container: document.getElementById('girl2'),
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
-    path: 'girl6.json'
-  })
+var charactors = ['girl1RTop','girl1LTop','girl2RTop','girl2LTop','girl3RTop'
+                         ,'girl3LTop','girl4RTop','girl4LTop','girl5RTop','girl5LTop',
+                  'girl1RSecond','girl1LSecond','girl2RSecond','girl2LSecond','girl3RSecond'
+                         ,'girl3LSecond','girl4RSecond','girl4LSecond','girl5RSecond','girl5LSecond',
+                  'girl1RThird','girl1LThird','girl2RThird','girl2LThird','girl3RThird'
+                         ,'girl3LThird','girl4RThird','girl4LThird','girl5RThird','girl5LThird',
+                  'girl1RFourth','girl1LFourth','girl2RFourth','girl2LFourth','girl3RFourth'
+                         ,'girl3LFourth','girl4RFourth','girl4LFourth','girl5RFourth','girl5LFourth',
+                  'girl1RFifth','girl1LFifth','girl2RFifth','girl2LFifth','girl3RFifth'
+                         ,'girl3LFifth','girl4RFifth','girl4LFifth','girl5RFifth','girl5LFifth'];
 
-  var girl3 = bodymovin.loadAnimation({
-    container: document.getElementById('girl3'),
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
-    path: 'girl6.json'
-  })
+ var jsonPaths = ['girl1R','girl1L','girl2R','girl2L','girl3R'
+                         ,'girl3L','girl4R','girl4L','girl5R','girl5L',
+                  'girl1R','girl1L','girl2R','girl2L','girl3R'
+                         ,'girl3L','girl4R','girl4L','girl5R','girl5L',
+                  'girl1R','girl1L','girl2R','girl2L','girl3R'
+                         ,'girl3L','girl4R','girl4L','girl5R','girl5L',
+                  'girl1R','girl1L','girl2R','girl2L','girl3R'
+                         ,'girl3L','girl4R','girl4L','girl5R','girl5L',
+                  'girl1R','girl1L','girl2R','girl2L','girl3R'
+                         ,'girl3L','girl4R','girl4L','girl5R','girl5L'];
 
-  var girl4 = bodymovin.loadAnimation({
-    container: document.getElementById('girl4'),
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
-    path: 'girl6.json'
-  })
+    var i, len = charactors.length;
+    for(i = 0; i < len; i += 1) {
+        var anim;
+        var elem = document.getElementById(charactors[i]);
+        var animData = {
+            container: elem,
+            renderer: 'svg',
+            loop: true,
+            autoplay: true,
+            path: 'walkers/' + jsonPaths[i] + '.json'
+        };
+        anim = lottie.loadAnimation(animData);
+        anim.setSubframe(false);
+    }
+  });
 
-  var girl5 = bodymovin.loadAnimation({
-    container: document.getElementById('girl5'),
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
-    path: 'girl6.json'
-  })
+// var girl1R = lottie.loadAnimation({
+//     container: document.getElementById('girl1R'),
+//     renderer: 'svg',
+//     loop: true,
+//     autoplay: true,
+//     path: 'walkers/girl1R.json'
+//   })
+  
+// var girl1L = lottie.loadAnimation({
+//     container: document.getElementById('girl1L'),
+//     renderer: 'svg',
+//     loop: true,
+//     autoplay: true,
+//     path: 'walkers/girl1L.json'
+//   })
 
-  var girl6 = bodymovin.loadAnimation({
-    container: document.getElementById('girl6'),
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
-    path: 'girl6.json'
-  })
+//   var girl2R = lottie.loadAnimation({
+//     container: document.getElementById('girl2R'),
+//     renderer: 'svg',
+//     loop: true,
+//     autoplay: true,
+//     path: 'walkers/girl2R.json'
+//   })
 
-  var girl7 = bodymovin.loadAnimation({
-    container: document.getElementById('girl7'),
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
-    path: 'girl6.json'
-  })
+//   var girl2L = lottie.loadAnimation({
+//     container: document.getElementById('girl2L'),
+//     renderer: 'svg',
+//     loop: true,
+//     autoplay: true,
+//     path: 'walkers/girl2L.json'
+//   })
 
-  var girl8 = bodymovin.loadAnimation({
-    container: document.getElementById('girl8'),
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
-    path: 'girl6.json'
-  })
+//   var girl3R = lottie.loadAnimation({
+//     container: document.getElementById('girl3R'),
+//     renderer: 'svg',
+//     loop: true,
+//     autoplay: true,
+//     path: 'walkers/girl3R.json'
+//   })
 
-  var girl9 = bodymovin.loadAnimation({
-    container: document.getElementById('girl9'),
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
-    path: 'girl6.json'
-  })
+//   var girl3L = lottie.loadAnimation({
+//     container: document.getElementById('girl3L'),
+//     renderer: 'svg',
+//     loop: true,
+//     autoplay: true,
+//     path: 'walkers/girl3L.json'
+//   })
 
-  var girl10 = bodymovin.loadAnimation({
-    container: document.getElementById('girl10'),
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
-    path: 'girl6.json'
-  })
+//   var girl4R = lottie.loadAnimation({
+//     container: document.getElementById('girl4R'),
+//     renderer: 'svg',
+//     loop: true,
+//     autoplay: true,
+//     path: 'walkers/girl4R.json'
+//   })
+
+//   var girl4L = lottie.loadAnimation({
+//     container: document.getElementById('girl4L'),
+//     renderer: 'svg',
+//     loop: true,
+//     autoplay: true,
+//     path: 'walkers/girl4L.json'
+//   })
+
+//   var girl5R = lottie.loadAnimation({
+//     container: document.getElementById('girl5R'),
+//     renderer: 'svg',
+//     loop: true,
+//     autoplay: true,
+//     path: 'walkers/girl5R.json'
+//   })
+
+//   var girl5L = lottie.loadAnimation({
+//     container: document.getElementById('girl5L'),
+//     renderer: 'svg',
+//     loop: true,
+//     autoplay: true,
+//     path: 'walkers/girl5L.json'
+//   })
